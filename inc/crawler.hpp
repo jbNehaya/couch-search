33046e56e58909defe63941fbf6da20fb2ad24ca
jbNehaya <jbNehaya96@gmail.com>
2024-08-14 21:39:23 +0300

    Crawler class not finished

---
+++ inc/crawler.hpp
@@ -0,0 +1,52 @@
+#ifndef CRAWLER_HPP_
+#define CRAWLER_HPP_
+
+#include<string>
+#include<unordered_set>
+#include<unordered_map>
+#include<set>
+
+#include"configuration.hpp"
+#include"downloader.hpp"
+#include "pageParser.hpp"
+
+class Crawler {
+public:
+
+    Crawler(Configuration const& a_config);
+
+    void start_crawling();
+    bool is_valid_link(std::string const& a_URL) const;
+    void save_in_DB();
+    void print_information() const;
+    void run();
+
+private:
+
+    void bfs_crawl(std::vector<std::string> const& a_startUrls); 
+    //void dfs_crawl(std::vector<std::string> const& a_startUrls); 
+    void crawl_page(std::string const& a_url, unsigned int a_depth);
+    bool should_skip_page(std::string const& a_url, unsigned int a_depth) const; 
+
+    std::string extract_domain(std::string const& a_url) const;
+
+
+private:
+
+    using unordered_map_page_with_count = std::unordered_map<std::string, unsigned int>;
+
+    unsigned int m_page_count;
+    unsigned int m_ignored_links_count;
+
+    const Configuration& m_config;
+    Downloader m_downloader;
+    PageParser m_parser;
+
+    std::unordered_set<std::string> m_visited_pages;
+    std::unordered_map<std::string, std::set<std::string>> m_page_linkes;
+    std::unordered_map<std::string, unordered_map_page_with_count > m_words_index;
+
+};
+
+#endif // CRAWLER_HPP_
+
---
+++ src/crawler.cpp
@@ -0,0 +1,112 @@
+#include"queue"
+#include <regex>
+#include "crawler.hpp"
+
+Crawler::Crawler(Configuration const& a_config)
+: m_page_count{0}
+, m_ignored_links_count{0}
+, m_config{a_config}
+{
+}
+
+void Crawler::start_crawling() 
+{
+    const auto& start_Urls = m_config.start_URLs();
+
+    if (m_config.crawl_mode() == CRAWL_MODE::BFS) {
+        bfs_crawl(start_Urls);
+    } /*else if (m_config.crawl_mode() == CRAWL_MODE::DFS) {
+        dfs_crawl(start_Urls);
+    }*/
+
+    print_information();
+}
+
+void Crawler::bfs_crawl(std::vector<std::string> const& a_startUrls) 
+{
+    std::queue<std::pair<std::string, unsigned int>> toVisit;
+
+    for (const auto& url : a_startUrls) {
+        toVisit.push({url, 0});
+    }
+
+    while (!toVisit.empty() && m_page_count < m_config.max_pages()) {
+        auto [url, depth] = toVisit.front();
+        toVisit.pop();
+
+        if (should_skip_page(url, depth)) {
+            continue;
+        }
+
+        crawl_page(url, depth);
+        
+       for (const auto& link : m_page_linkes[url]) {
+            if (is_valid_link(link)) {
+                toVisit.push({link, depth + 1});
+            } else {
+                ++m_ignored_links_count;
+            }
+        }
+    }
+}
+
+void Crawler::crawl_page(std::string const& a_url, unsigned int a_depth)
+{
+    auto content = m_downloader.download_url_content(a_url);
+    m_parser.parsing(a_url);
+    m_page_linkes[a_url] =  m_parser.page_links();
+    m_words_index[a_url] = m_parser.word_index();
+    m_visited_pages.insert(a_url);
+    ++m_page_count;
+}
+
+bool Crawler::should_skip_page(std::string const& a_url, unsigned int a_depth) const 
+{
+    if (m_visited_pages.find(a_url) != m_visited_pages.end()) {
+        return true;
+    }
+
+    if (a_depth > m_config.max_depth()) {
+        return true;
+    }
+
+    if (m_config.max_pages() > 0 && m_page_count >= m_config.max_pages()) {
+        return true;
+    }
+
+    return false;
+}
+
+std::string Crawler::extract_domain(std::string const& a_url) const
+{
+    std::regex domain_regex(R"(https?://([^/]+))");
+    std::smatch match;
+    if (std::regex_search(a_url, match, domain_regex)) {
+        return match[1].str();
+    }
+    return "";
+}
+
+bool Crawler::is_valid_link(std::string const& a_URL) const
+{
+    std::string current_url_domain = extract_domain(a_URL);
+    const auto& start_Urls = m_config.start_URLs();
+    
+    size_t size = start_Urls.size();
+    for(size_t i=0; i< size; ++i){
+        std::string start_url_domain = extract_domain(start_Urls[i]);
+        if(current_url_domain == start_url_domain){
+            return true;
+        }
+    }
+    return false;
+}
+
+void Crawler::print_information() const
+{
+    std::cout << "Crawling finished \n";
+    std::cout << "Counts of words indexed: " << m_words_index.size() << "\n";
+    std::cout << "Counts of pages crawled: " << m_page_count << "\n";
+    std::cout << "Counts of ignored links: " << m_ignored_links_count << "\n";
+}
+
