154cd1998ddfc75109ad0d4cb01e33979d769e28
jbNehaya <jbNehaya96@gmail.com>
2024-08-15 17:28:05 +0300

    remove the print debug

--- src/downloader.cpp
+++ src/downloader.cpp
@@ -12,7 +12,7 @@ std::string Downloader::download_url_content(std::string const& a_URL)
         m_request.setOpt(curlpp::options::WriteStream(&response));
         m_request.perform();
 
-        std::cout<<"Content: " << response.str() << "\n";
+        //std::cout<<"Content: " << response.str() << "\n";
 
     } catch(curlpp::RuntimeError &e){
         std::cerr << "Runtime error: " << e.what() << "\n";
