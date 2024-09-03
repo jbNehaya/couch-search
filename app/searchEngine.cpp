5b997134bb1e17698b3760004791f6ac908a1dc1
jbNehaya <jbNehaya96@gmail.com>
2024-09-03 21:12:23 +0300

    now Crawler is an abstract class, so update it initialization

--- app/searchEngine.cpp
+++ app/searchEngine.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
 #include "crawler.hpp"
 #include "configuration.hpp"
+#include <fstream>
+#include <nlohmann/json.hpp>
 #include"query.hpp"
+#include"BFSCrawler.hpp"
+#include"DFSCrawler.hpp"
+
+void save_data(const std::unordered_map<std::string, std::unordered_map<std::string, unsigned int>>& words_index,
+               const std::unordered_map<std::string, std::string>& page_titles) 
+{
+    nlohmann::json json_data;
+    json_data["words_index"] = words_index;
+    json_data["page_titles"] = page_titles;
+    std::ofstream file("data.json");
+    file << json_data.dump();
+}
 
 int main() 
 {
     try {
         Configuration config("configuration.json");
-        Crawler crawler(config);
 
-        crawler.start_crawling();
-        Query query(crawler.words_index(), crawler.page_titles(), 10); 
+        std::unique_ptr<Crawler> crawler;
+        if(config.crawl_mode() == CRAWL_MODE::BFS) {
+            crawler = std::make_unique<BFSCrawler>(config);
+        }/* else if (config.crawl_mode() == CRAWL_MODE::DFS) {
+            crawler = std::make_unique<DFSCrawler>(config);
+        } */else {
+            crawler = std::make_unique<BFSCrawler>(config);
+        }
+
+        crawler->start_crawling();
+        save_data(crawler->words_index(), crawler->page_titles());
+        
+        Query query(crawler->words_index(), crawler->page_titles(), 10); 
+
 
         std::string search_term;
         std::cout << "Crawling is complete. You can now search for terms.\n";
