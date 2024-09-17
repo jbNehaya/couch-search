84454db870442702c0e7dfd00f66758620e36c36
jbNehaya <jbNehaya96@gmail.com>
2024-09-17 18:52:55 +0300

    now the class in no longer abstract class, its concrete class, it inherits from ICrawler(interface ) class

--- inc/crawler.hpp
+++ inc/crawler.hpp
@@ -5,25 +5,69 @@
 #include<unordered_set>
 #include<unordered_map>
 #include<set>
+#include <thread>
+#include<mutex>
+#include<condition_variable>
 #include"configuration.hpp"
 #include"downloader.hpp"
 #include "pageParser.hpp"
+#include "ICrawler.hpp"
 
-class Crawler {
+class Crawler : public ICrawler {
 public:
+
+    Crawler(Configuration const& a_config);
     virtual ~Crawler() = default;
 
-    virtual void start_crawling() = 0;
-    virtual std::unordered_map<std::string, std::unordered_map<std::string, unsigned int>> words_index() const = 0;
-    virtual std::unordered_map<std::string, std::string> page_titles() const = 0;
+    virtual void start_crawling() override;
+    std::unordered_map<std::string, std::unordered_map<std::string, unsigned int>> words_index() const;
+    std::unordered_map<std::string, std::string> page_titles() const;
 
 protected:
+
+    virtual void crawl_using_specific_mode(std::vector<std::string> const& a_startUrls) = 0;
     virtual void crawl_page(std::string const& a_url, unsigned int a_depth) = 0;
-    virtual bool should_skip_page(std::string const& a_url, unsigned int a_depth) const = 0;
-    virtual std::string extract_domain(std::string const& a_url) const = 0;
-    virtual bool is_valid_link(std::string const& a_URL) const = 0;
-    virtual void print_information() const = 0;
+    virtual void worker_thread() = 0;
+    bool should_skip_page(std::string const& a_url, unsigned int a_depth) const;
+    std::string extract_domain(std::string const& a_url) const;
+    bool is_valid_link(std::string const& a_URL) const;
+    void print_information() const;
+
+protected:
+    
+    unsigned int m_page_count;
+    unsigned int m_ignored_links_count;
+    Configuration const& m_config;
+    Downloader m_downloader;
+    PageParser m_parser;
+    std::unordered_set<std::string> m_visited_pages;
+    std::unordered_map<std::string, std::set<std::string>> m_page_linkes;
+    std::unordered_map<std::string, std::unordered_map<std::string, unsigned int>> m_words_index;
+    std::unordered_map<std::string, std::string> m_page_titles;
+
+    std::mutex m_mutex;
+    std::condition_variable m_cond_var;
+    bool m_done = false;
+    std::vector<std::thread> m_threads;
     
 };
 
+/*
+class ICrawler {
+public:
+    virtual void start() = 0;
+};
+
+
+template <typename SearchPolicy>
+class Crawler {
+public:
+    void start() override {
+        SerachPolicy container;
+        
+    }
+};
+*/
 #endif // CRAWLER_HPP_
+
+
